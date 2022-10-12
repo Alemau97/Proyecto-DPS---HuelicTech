aa7a46aa57418893a2c5e0717d03ce656f74ee5d
TuTioElPollo <gamerbloom5@gmail.com>
2022-10-12 15:47:08 -0600

    Subiendo Splash Screen

---
+++ Huelic-Tech/src/screens/PrincipalScreen.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  ScrollView,
+  Pressable,
+} from "react-native";
+
+export default class LoginScreen extends Component {
+  render() {
+    return (
+      <Text>Bienvenido a Huelic</Text>
+    )
+  }
+}
