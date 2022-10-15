6be7be2265d7d0e91a3ca77cfddcb38bfcf70e32
Carlos Portillo <carloskportillo@gmail.com>
2022-10-14 23:52:31 -0600

    Homepage en progreso

--- Huelic-Tech/src/navigation/AppNavigation.js
+++ Huelic-Tech/src/navigation/AppNavigation.js
@@ -1,39 +1,36 @@
-import {createAppContainer} from 'react-navigation'
-import { createStackNavigator } from 'react-navigation-stack'
-import SplashScreen from '@screens/SplashScreen'
-import {MainScreen} from '@screens/MainScreen' 
-import { RegisterScreen } from '@screens/RegisterScreen'
-import Header from '../layout/Header'
-
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import SplashScreen from "@screens/SplashScreen";
+import { MainScreen } from "@screens/MainScreen";
+import { RegisterScreen } from "@screens/RegisterScreen";
+import { Homepage } from "@components/Homepage";
 
 const AppNavigation = createStackNavigator({
-
-    Splash:{
-        screen: SplashScreen,
-        navigationOptions:{
-            headerShown: false,
-        }
+  Splash: {
+    screen: SplashScreen,
+    navigationOptions: {
+      headerShown: false,
     },
+  },
 
-    Main:{
-        screen: MainScreen,
-        navigationOptions:{
-            headerShown: false,
-        }
+  Main: {
+    screen: MainScreen,
+    navigationOptions: {
+      headerShown: false,
     },
-    Register:{
-        screen: RegisterScreen,
-        navigationOptions:{
-            headerShown: false,
-        }
+  },
+  Register: {
+    screen: RegisterScreen,
+    navigationOptions: {
+      headerShown: false,
     },
-    Header:{
-        screen: Header,
-        navigationOptions:{
-            headerShown: false,
-        }
-    }
+  },
+  Header: {
+    screen: Homepage,
+    navigationOptions: {
+      headerShown: false,
+    },
+  },
+});
 
-})
-
-export default createAppContainer(AppNavigation)
+export default createAppContainer(AppNavigation);
--- Huelic-Tech/src/screens/SplashScreen.js
+++ Huelic-Tech/src/screens/SplashScreen.js
@@ -1,36 +1,39 @@
-import React, {Component} from 'react'
-import {View, StatusBar} from 'react-native'
-import * as Animatable from 'react-native-animatable'
-import {imageBackgroundStyle} from '@styles/General'
+import React, { Component } from "react";
+import { View, StatusBar } from "react-native";
+import * as Animatable from "react-native-animatable";
+import { imageBackgroundStyle } from "@styles/General";
 
-export default class LoginScreen extends Component{
-    goToScreen(routeName){
-        this.props.navigation.navigate(routeName)
-    }
+export default class LoginScreen extends Component {
+  goToScreen(routeName) {
+    this.props.navigation.navigate(routeName);
+  }
 
-    componentDidMount(){
-        setTimeout( ()=>{
-            this.goToScreen('Main')
-        }, 2000, this)
-    }
+  componentDidMount() {
+    setTimeout(
+      () => {
+        this.goToScreen("Header");
+      },
+      2000,
+      this
+    );
+  }
 
-    render(){
-        return(
-           <View style={imageBackgroundStyle.image}>
-               <StatusBar translucent backgroundColor='rgba(0,0,0,0.2)'/>
-               <Animatable.Image
-                   animation="slideInDown"
-                   easing="ease-out"
-                   iterationCount={1}
-                   style={{
-                       width: 320,
-                       height: 80,
-                       margin: 100,
-                   }}
-                   source={require('@recursos/images/LogoHuelic.png')}
-               />
-           </View>
-        )
-        
-    }
+  render() {
+    return (
+      <View style={imageBackgroundStyle.image}>
+        <StatusBar translucent backgroundColor="rgba(0,0,0,0.2)" />
+        <Animatable.Image
+          animation="slideInDown"
+          easing="ease-out"
+          iterationCount={1}
+          style={{
+            width: 320,
+            height: 80,
+            margin: 100,
+          }}
+          source={require("@recursos/images/LogoHuelic.png")}
+        />
+      </View>
+    );
+  }
 }
