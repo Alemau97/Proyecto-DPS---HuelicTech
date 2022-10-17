b8d2ce0bf2f94bd69944f23a0825c5feb50c7734
Carlos Portillo <carloskportillo@gmail.com>
2022-10-16 20:14:03 -0600

    Dando diseño a homescreen

--- Huelic-Tech/src/layout/Tabs.js
+++ Huelic-Tech/src/layout/Tabs.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+import { StyleSheet } from "react-native";
 
 import { Homepage } from "../components/Homepage";
 import { Componentes } from "../screens/Componentes";
 import { Accesorios } from "../screens/Accesorios";
+import { Contacto } from "../screens/Contacto";
 
 const Tab = createBottomTabNavigator();
 
@@ -14,22 +17,42 @@ function HomeTabs() {
       <Tab.Screen
         name="Homescreen"
         component={Homepage}
-        options={{ headerShown: false }}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="home" color={color} size={size} />
+          ),
+        }}
       />
       <Tab.Screen
         name="Componentes"
         component={Componentes}
-        options={{ headerShown: false }}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="monitor" color={color} size={size} />
+          ),
+        }}
       />
       <Tab.Screen
         name="Accesorios"
         component={Accesorios}
-        options={{ headerShown: false }}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="keyboard" color={color} size={size} />
+          ),
+        }}
       />
       <Tab.Screen
         name="Contacto"
-        component={Homepage}
-        options={{ headerShown: false }}
+        component={Contacto}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="chat" color={color} size={size} />
+          ),
+        }}
       />
     </Tab.Navigator>
   );
@@ -38,3 +61,5 @@ function HomeTabs() {
 export default function Tabs() {
   return <HomeTabs />;
 }
+
+const styles = StyleSheet.create({});
