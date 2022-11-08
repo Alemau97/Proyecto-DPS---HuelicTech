f37af5757380a8154b9bb6a7d27853530ad7748a
TuTioElPollo <gamerbloom5@gmail.com>
2022-11-08 12:07:19 -0600

    Agregando Nuevos Productos

--- Huelic-Tech/src/components/NuevosProductos.js
+++ Huelic-Tech/src/components/NuevosProductos.js
@@ -7,67 +7,139 @@ import { Modall } from './Modall';
 
 const icon = [
   {
-    img: require("../images/xbox.jpg"),
-    title: 'xbox'
+    img: require("../images/newProd/nuevo1.jpeg"),
+    title: 'CASE COOLER MASTER MB520 ARGB MCB-B520-KGNN-RGA'
   },
   {
-    img: require("../images/xbox.jpg"),
-    title: 'xbox'
+    img: require("../images/newProd/nuevo2.jpeg"),
+    title: 'VENTILADOR COOLER MASTER SICKLEFLOW'
   },
   {
-    img: require("../images/xbox.jpg"),
-    title: 'xbox'
+    img: require("../images/newProd/nuevo3.jpeg"),
+    title: 'LENTES DE REALIDAD VIRTIUAL VR FUTURE'
   },
   {
-    img: require("../images/xbox.jpg"),
-    title: 'xbox'
+    img: require("../images/newProd/nuevo4.jpeg"),
+    title: 'MICROSOFT XBOX SERIES X 120FPS 4K 8K HDR 1TB SSD VRR'
   },
   {
-    img: require("../images/xbox.jpg"),
-    title: 'xbox'
+    img: require("../images/newProd/nuevo5.jpeg"),
+    title: 'MICROSOFT XBOX SERIES S SSD 512GB VRR HDR'
   },
   {
-    img: require("../images/xbox.jpg"),
-    title: 'xbox'
+    img: require("../images/newProd/nuevo6.jpeg"),
+    title: 'CONTROL INALÁMBRICO XBOX SERIES X/S/ONE BLACK'
+  },
+  {
+    img: require("../images/newProd/nuevo7.jpeg"),
+    title: 'CONTROL DUALSHOCK PS4 ROJO MAGMA'
+  },
+  {
+    img: require("../images/newProd/nuevo8.jpeg"),
+    title: 'HORIZON ZERO DAWN FORBIDDEN WEST PS5'
+  },
+  {
+    img: require("../images/newProd/nuevo9.jpeg"),
+    title: 'ENFRIAMIENTO LIQUIDO COOLER MASTER MASTERLIQUID LITE 120'
+  },
+  {
+    img: require("../images/newProd/nuevo10.jpeg"),
+    title: 'LAPTOP ASUS TUF DASH FX516PM-211-TF15-11 PROC.'
+  },
+  {
+    img: require("../images/newProd/nuevo11.jpeg"),
+    title: 'MONITOR AOC FULLHD 22P 22B2HM 75Hz VA HDMI'
+  },
+  {
+    img: require("../images/newProd/nuevo12.jpeg"),
+    title: 'ROUTER TP-LINK 4G LTE 300MBPS TL-MR100'
   },
 ]
 
 const modal = [
   {
-    name: 'Producto 1',
-    brand: 'Marca 1',
-    descp: 'Descripcion 1',
-    volver: 'Ver más productos'
+    name: 'CASE COOLER MASTER MB520 ARGB MCB-B520-KGNN-RGA',
+    brand: 'COOLER MASTER',
+    descp: 'Tarjetas madre soportadas: ATX, EATX, Micro ATX, Mini-ITX, SSI CEB > Tipo: PC, Peso: 6.9 kg, Peso del paquete: 8 kg, NO INCLUYE FUENTE',
+    volver: 'Ver más productos',
+    precio: '$125.00'
   },
   {
-    name: 'Producto 2',
-    brand: 'Marca 2',
-    descp: 'Descripcion 2',
-    volver: 'Ver más productos'
+    name: 'VENTILADOR COOLER MASTER SICKLEFLOW  ARGB FAN MFX-B2DN-183PA-R1 (1212151)',
+    brand: 'COOLER MASTER',
+    descp: 'VENTILADOR COOLER MASTER SICKLEFLOW ARGB FAN MFX-B2DN-183PA-R1 Tipo de soporte Rodamiento tipo rifle Velocidad de rotación (mín.) 650 RPM Velocidad de rotación (máx.) 1800 RPM Presión máxima de aire 2,5 mmH2O Nivel de ruido (baja velocidad) 8 dB Nivel de ruido (alta velocidad) 27 dB Diámetro de ventilador 12 cm Localización adecuada Carcasa del ordenador ',
+    volver: 'Ver más productos',
+    precio: '$65.00'
   },
   {
-    name: 'Producto 3',
-    brand: 'Marca 3',
-    descp: 'Descripcion 3',
-    volver: 'Ver más productos'
+    name: 'LENTES DE REALIDAD VIRTIUAL VR FUTURE',
+    brand: 'VR FUTURE',
+    descp: 'No descripción',
+    volver: 'Ver más productos',
+    precio: '$20.00'
   },
   {
-    name: 'Producto 4',
-    brand: 'Marca 4',
-    descp: 'Descripcion 4',
-    volver: 'Ver más productos'
+    name: 'MICROSOFT XBOX SERIES X 120FPS 4K 8K HDR 1TB SSD VRR',
+    brand: 'XBOX',
+    descp: 'No descripción',
+    volver: 'Ver más productos',
+    precio: '$850.00'
   },
   {
-    name: 'Producto 5',
-    brand: 'Marca 5',
+    name: 'MICROSOFT XBOX SERIES S SSD 512GB VRR HDR FORTNITE ROCKET LEAGUE BUNDLE M1883',
+    brand: 'XBOX',
+    descp: 'No descripción',
+    volver: 'Ver más productos',
+    precio: '$450.00'
+  },
+  {
+    name: 'CONTROL INALÁMBRICO XBOX SERIES X/S/ONE BLACK',
+    brand: 'XBOX',
     descp: 'Descripcion 5',
-    volver: 'Ver más productos'
+    volver: 'Incluye control + cable USB-C',
+    precio: '$89.99'
   },
   {
-    name: 'Producto 6',
-    brand: 'Marca 6',
-    descp: 'Descripcion 6',
-    volver: 'Ver más productos'
+    name: 'CONTROL DUALSHOCK PS4 ROJO MAGMA',
+    brand: 'PLAY STATION',
+    descp: 'Da rienda suelta a tu color con la llegada de cuatro nuevos estilos. Cada controlador inalámbrico viene cargado con las mismas características de DUALSHOCK 4 que incluyen panel táctil, sensores de movimiento y batería recargable incorporada. Los sticks analógicos evolucionados y los botones de disparo permiten una precisión sin igual en cada movimiento, mientras que las tecnologías innovadoras ofrecen formas emocionantes de experimentar tus juegos y compartir tus mejores momentos.',
+    volver: 'Ver más productos',
+    precio: '$75.00'
+  },
+  {
+    name: 'HORIZON ZERO DAWN FORBIDDEN WEST PS5',
+    brand: 'GUERRILLA',
+    descp: 'La segunda parte de este grandioso juego que nos llevó a un mundo apocalíptico rodeado de máquinas. ',
+    volver: 'Ver más productos',
+    precio: '$85.00'
+  },
+  {
+    name: 'ENFRIAMIENTO LIQUIDO COOLER MASTER MASTERLIQUID LITE 120 MLW-D12M-A20PW-R1',
+    brand: 'COOLER MASTER',
+    descp: 'ENFRIAMIENTO LIQUIDO COOLER MASTER MASTERLIQUID LITE MLW-D12M-A20PW-R1 Socket compatibles Intel LGA 2011-v3 / 2011 / 1366 / 1151 / 1150 / 1156 / 1155 / 775 socket AMD AM4 / AM3+ / AM3 / AM2+ / AM2 / FM2+ / FM2 /',
+    volver: 'Ver más productos',
+    precio: '$72.00'
+  },
+  {
+    name: 'LAPTOP ASUS TUF DASH FX516PM-211-TF15-11 PROC.',
+    brand: 'ASUS',
+    descp: 'INTEL CORE i7 11370H RAM 16GB ALMACENAMIENTO SSD 512GB RTX3060 6GB PANTALLA FHD 15.6P W11 WIFI6+BT',
+    volver: 'Ver más productos',
+    precio: '$1625.00'
+  },
+  {
+    name: 'MONITOR AOC FULLHD 22P 22B2HM 75Hz VA HDMI',
+    brand: 'AOC',
+    descp: 'Pantalla con elegante diseño sin marco que mejora la experiencia de visión, óptimo para setups con múltiples monitores.',
+    volver: 'Ver más productos',
+    precio: '$200.00'
+  },
+  {
+    name: 'ROUTER TP-LINK 4G LTE 300MBPS TL-MR100',
+    brand: 'tp-link',
+    descp: 'Red 4G de vanguardia, Inserta una tarjeta SIM , Dos antenas LTE avanzadas desmontables, Modo router Wi-Fi ',
+    volver: 'Ver más productos',
+    precio: '$92.00'
   },
 ]
 
@@ -77,23 +149,26 @@ export const NuevosProductos = () => {
     <>
       <Header />
       <View style={styles.mainContainer}>
-        <View style={styles.container}>
-          {
-            icon.map((data, key)=>{
-              return(
-                <View style={styles.sections} key={key}>
-                  <Modall 
-                    img = {data.img}
-                    title = {data.title}
-                    nombre = {modal[key].name}
-                    marca = {modal[key].brand}
-                    descp = {modal[key].descp}
-                    volver = {modal[key].volver}
-                  />
-                </View>
-              )
-            })
-          }
+      <View>
+          <ScrollView contentContainerStyle={styles.container}>
+            {
+              icon.map((data, key)=>{
+                return(
+                  <View style={styles.sections} key={key}>
+                    <Modall 
+                      img = {data.img}
+                      title = {data.title}
+                      nombre = {modal[key].name}
+                      marca = {modal[key].brand}
+                      descp = {modal[key].descp}
+                      volver = {modal[key].volver}
+                      precio = {modal[key].precio}
+                    />
+                  </View>
+                )
+              })
+            }
+          </ScrollView>
         </View>
       </View>
     </>
